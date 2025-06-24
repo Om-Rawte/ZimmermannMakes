8486f4e345314ee5173192bed64f6f974d1cde1a
Om Rawte <thepsychsage09@gmail.com>
2025-06-24 02:01:33 +0200

    Add files via upload

--- netlify/functions/tables.js
+++ netlify/functions/tables.js
@@ -1,14 +1,7 @@
+// This function is deprecated. Fetch tables directly from Supabase in the frontend.
 exports.handler = async function(event, context) {
   return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify([
-      { id: 1, name: "Table 1", capacity: 4, available: true },
-      { id: 2, name: "Table 2", capacity: 2, available: false },
-      { id: 3, name: "Table 3", capacity: 6, available: true }
-    ])
+    statusCode: 410,
+    body: JSON.stringify({ message: 'Deprecated. Fetch tables from Supabase.' })
   };
 }; 
